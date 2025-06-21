992535beb52993d6a847abd66cc2e1125cc7bf28
LeaoMartelo <leaomartelo@gmail.com>
2025-06-21 14:18:48 -0300

    fix index

--- js/index.js
+++ js/index.js
@@ -15,3 +15,11 @@ window.addEventListener('click', function(event) {
     modal.style.display = 'none';
   }
 });
+
+
+document.querySelectorAll('.big-rounded-button').forEach(function(button) {
+    button.addEventListener('click', function() {
+        var linkUrl = this.getAttribute('data-link');
+        window.location.href = linkUrl;
+    });
+});
